a000532a2378b30a7091909cea0c91461c7c6d1c
Stelle <134593187+hokojons@users.noreply.github.com>
2025-06-09 12:02:07 +0700

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section className="home">
+      <h1>Welcome to My React App</h1>
+      <p>Use the navigation above to switch between pages.</p>
+    </section>
+  )
+}
